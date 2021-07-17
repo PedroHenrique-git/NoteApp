88d1ca9e8ec9fe8cc93bbdaa3951b4a18d16dd8e
PedroHenrique-git <pedromf34@outlook.com>
2021-07-16 22:04:00 -0300

    ajustes

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,11 +1,18 @@
 export const modal = document.querySelector('.add_task-modal');
 
+const isMobile = () => {
+    if(window.innerWidth <= 640) return true;
+    return false;
+}
+
 export const showModal = () => {
     modal.style.display = 'flex';
+    if( isMobile ) document.querySelector('body').style.overflowY = 'hidden';
 };
 
 export const hideModal = () => {
-    modal.style.display = 'none';    
+    modal.style.display = 'none'; 
+    if( isMobile ) document.querySelector('body').style.overflowY = 'auto';   
 };
 
 export const setTargetList = (targeList) => {
